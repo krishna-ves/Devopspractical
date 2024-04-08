add5bd0558eb4f8921609331c58189533f0e1464
krishna-ves <159638192+krishna-ves@users.noreply.github.com>
2024-04-08 12:57:38 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,23 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers from 1 to 100:");
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    
+    // Function to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
